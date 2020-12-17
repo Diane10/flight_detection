0f77aac51d4b590cebbb44f5184d9fecae17c042
Diane10 <46956585+Diane10@users.noreply.github.com>
2020-12-17 13:24:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 st.title("Flight Delaying Detector")
     # st.subheader("ML App with Streamlit")
 html_temp = """
@@ -7,12 +9,20 @@ html_temp = """
     </div>
     """
 st.markdown(html_temp,unsafe_allow_html=True)
- 
-AIRLINE= st.text_input("AIRLINE","Type Here",key='1')
-ORIGIN_AIRPORT= st.text_input("ORIGIN_AIRPORT","Type Here",key='2')
-DESTINATION_AIRPORT=st.text_input("DESTINATION_AIRPORT","Type Here",key='3')
-SCHEDULED_DEPARTURE= st.text_input("SCHEDULED_DEPARTURE","Type Here",key='4')
-DEPARTURE_TIME= st.text_input("DEPARTURE_TIME","Type Here",key='5')
-DEPARTURE_DELAY= st.text_input("DEPARTURE_DELAY","Type Here",key='6')
-SCHEDULED_ARRIVAL= st.text_input("SCHEDULED_ARRIVAL","Type Here",key='7')
-ARRIVAL_TIME= st.text_input("ARRIVAL_TIME","Type Here",key='8')
+ 'ORIGIN', 'DEST', 'DISTANCE', 'Carrier mean distance',
+        'Origin Delay', 'Origin TaxiOut']]
+model=pickle.load(open('models.pkl','rb'))        
+MONTH= st.text_input("MONTH","Type Here",key='1')
+DAY_OF_MONTH= st.text_input("DAY_OF_MONTH","Type Here",key='2')
+DAY_OF_WEEK=st.text_input("DAY_OF_WEEK","Type Here",key='3')
+CRS_DEP_TIME= st.text_input("CRS_DEP_TIME","Type Here",key='4')
+ORIGIN= st.text_input("ORIGIN","Type Here",key='5')
+DEST= st.text_input("DEST","Type Here",key='6')
+DISTANCE= st.text_input("DISTANCE","Type Here",key='7')
+Carriermeandistance= st.text_input("Carrier mean distance","Type Here",key='8')
+OriginDelay= st.text_input("Origin Delay","Type Here",key='9')
+OriginTaxiOut= st.text_input("Origin TaxiOut","Type Here",key='10')
+if st.button("predict"):
+   input=np.array([[MONTH,DAY_OF_MONTH,DAY_OF_WEEK, CRS_DEP_TIME,ORIGIN,DEST,DISTANCE,Carriermeandistance,OriginDelay,OriginTaxiOut]]).astype(np.float64)
+   prediction=model.predict(input)
+    
